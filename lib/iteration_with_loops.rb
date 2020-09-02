2416e2715b4f6714978ac49cc72d38ab83bf4bc1
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-02 23:06:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,7 +13,7 @@ def find_min_in_nested_arrays(src)
     smallest_number_element = ""
     
     while element_index < src[row_index].count do
-      src.sort!
+      src[row_index].sort!
       if src[row_index][element_index] <= src[row_index][0]
         
        smallest_number_element = src[row_index][element_index]
